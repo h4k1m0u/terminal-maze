3bd95fb2f850e2094c478cd9561d0e7a7750f25a
Hakim <h.benoudjit@gmail.com>
2025-02-02 10:57:30 +0100

    Ensure shell prompt doesn't step on printed maze

--- maze.py
+++ maze.py
@@ -1,7 +1,9 @@
-from terminal import clear_screen
+from terminal import clear_screen, move_cursor
 from grid import Grid
 
 clear_screen()
 grid = Grid()
 grid.render()
-print()
+
+# ensure next shell prompt doesn't step on printed maze
+move_cursor(Grid.N_ROWS, 0)
--- terminal.py
+++ terminal.py
@@ -4,9 +4,13 @@
 def move_cursor_to_origin():
     print('\x1b[H')
 
-def print_at_coords(letter, row, col):
-    """Origin is (1, 1) while coords in grid.py are zero-indexed"""
-    print(f'\x1b[{row + 1};{col + 1}H' + letter, end='')
+def move_cursor(row: int, col: int):
+    """Origin is (1, 1) with escape characters while coords in grid.py are zero-indexed (same as passed arguments)"""
+    print(f'\x1b[{row + 1};{col + 1}H', end='')
+
+def print_at_coords(letter: str, row: int, col: int):
+    move_cursor(row, col)
+    print(letter, end='')
 
 def clear_screen():
     print('\x1b[2J', end='')
